c2a40f83b42c4bb98af1457cdc28729ad42c5dca
Ethan <elc6696@rit.edu>
2024-04-17 11:51:01 -0400

    reading files work for hoppers

--- src/puzzles/hoppers/model/HoppersConfig.java
+++ src/puzzles/hoppers/model/HoppersConfig.java
@@ -2,13 +2,43 @@ package puzzles.hoppers.model;
 
 import puzzles.common.solver.Configuration;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Collection;
 
 // TODO: implement your HoppersConfig for the common solver
 
 public class HoppersConfig implements Configuration{
+
+    private static int rows;
+    private static int cols;
+    private char grid[][];
+
     public HoppersConfig(String filename) throws IOException {
+
+        try (BufferedReader in = new BufferedReader(new FileReader(filename))){
+
+            String cords = in.readLine();
+
+            String[] fields = cords.split("\\s+");
+
+            this.rows = Integer.parseInt(fields[0]);
+            this.cols = Integer.parseInt(fields[1]);
+            this.grid = new char[rows][cols];
+
+            for (int r = 0; r < rows; r++) {
+
+                String line = in.readLine();
+                String[] lines = line.split("\\s");
+
+                for (int c = 0; c < cols; c++) {
+
+                    this.grid[r][c] = lines[c].charAt(0);
+
+                }
+            }
+        }
     }
 
     @Override
@@ -19,6 +49,18 @@ public class HoppersConfig implements Configuration{
     @Override
     public Collection<Configuration> getNeighbors() {
         return null;
+    }
+
+    public int getRows() {
+
+        return this.rows;
+
+    }
+
+    public int getCols() {
+
+        return this.cols;
+
     }
     
     @Override
@@ -31,6 +73,22 @@ public class HoppersConfig implements Configuration{
 
     @Override
     public String toString() {
+
         return null;
+
+//        for (int row=0; row<getRows(); ++row) {
+//            result.append(row).append(VERT_WALL);
+//            // this loop is for the row of cells and the horizontal connections
+//            for (int col = 0; col < getCols(); ++col) {
+//                // get the cell value to display for this coord
+//                result.append(getCell(new Coordinates(row, col)));
+//                if (col < getCols() - 1) {
+//                    if (isNeighbor(new Coordinates(row, col), new Coordinates(row, col + 1))) {
+//                        result.append(EMPTY);
+//                    } else {
+//                        result.append(VERT_WALL);
+//                    }
+//                }
+//            }
     }
 }
--- src/puzzles/hoppers/solver/Hoppers.java
+++ src/puzzles/hoppers/solver/Hoppers.java
@@ -1,11 +1,28 @@
 package puzzles.hoppers.solver;
 
 import puzzles.common.solver.Solver;
+import puzzles.hoppers.model.HoppersConfig;
+
+import java.io.IOException;
 
 public class Hoppers {
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage: java Hoppers filename");
+        } else {
+
+            String filename = args[0];
+
+            try {
+
+                HoppersConfig startConfig = new HoppersConfig(filename);
+                System.out.println(startConfig);
+
+
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+
         }
     }
 }
