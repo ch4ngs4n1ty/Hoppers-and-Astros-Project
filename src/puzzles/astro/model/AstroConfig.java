dbf89d52ef41f80d464f939d191429c4b1b06a69
Ashwin Jagadeesh <aj9596@rit.edu>
2024-04-17 11:50:45 -0400

    completed reading the file

--- src/puzzles/astro/model/AstroConfig.java
+++ src/puzzles/astro/model/AstroConfig.java
@@ -1,14 +1,85 @@
 package puzzles.astro.model;
 
+import puzzles.common.Coordinates;
 import puzzles.common.solver.Configuration;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.HashMap;
 
 // TODO: implement your HoppersConfig for the common solver
 
 public class AstroConfig implements Configuration{
+
+    private static int rows;
+    private static int cols;
+
+    private String[][] grid;
+
+    private static Coordinates home;
+    private Coordinates astonautLoc;
+
+    private HashMap<String, Coordinates> robots;
+
     public AstroConfig(String filename) throws IOException {
+
+        try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
+            String line = in.readLine();
+
+            String[] coordinates = line.split("\\s");
+
+            this.rows = Integer.parseInt(coordinates[0]);
+            this.cols = Integer.parseInt(coordinates[1]);
+
+            System.out.println(rows);
+            System.out.println(cols);
+
+            this.grid = new String[rows][cols];
+
+            //creates grid with the row and col dimensions
+            for (int row = 0; row < this.rows; row++) {
+                for (int col = 0; col < this.cols; col++) {
+                    grid[row][col] = String.valueOf(0);
+                }
+            }
+
+            String[] goalLocation = in.readLine().split("\\s+");
+            this.home = new Coordinates(String.valueOf(goalLocation[1].charAt(0)), String.valueOf(goalLocation[1].charAt(2)));
+            System.out.println(home);
+
+
+            String[] astronaut = in.readLine().split("\\s+");
+            this.astonautLoc = new Coordinates(String.valueOf(astronaut[1].charAt(0)), String.valueOf(astronaut[1].charAt(2)));
+            System.out.println(astonautLoc);
+
+
+            // create a hashmap of robot name to their coordinates
+            int numRobots = Integer.parseInt(in.readLine());
+
+            robots = new HashMap<>();
+            for(int i = 0; i < numRobots; i++){
+                String[] robotFields = in.readLine().split(" ");
+                Coordinates currentRobot = new Coordinates(String.valueOf(robotFields[1]));
+                robots.put(robotFields[0], currentRobot);
+            }
+            System.out.println(robots);
+
+
+
+        }
+
+    }
+
+    public int getRows(){
+        return this.rows;
+    }
+    public int getCols(){
+        return this.cols;
+    }
+    public String getVal(int row, int col) {
+        return grid[row][col];
     }
 
     @Override
@@ -31,6 +102,18 @@ public class AstroConfig implements Configuration{
 
     @Override
     public String toString() {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (int row=0; row<getRows(); ++row) {
+            for (int col=0; col<getCols(); ++col) {
+                result.append(getVal(row, col));
+                if (col<getCols()-1) {
+                    result.append(" ");
+                }
+            }
+            result.append(System.lineSeparator());
+        }
+
+        return result.toString();
+        //return null;
     }
 }
--- src/puzzles/astro/solver/Astro.java
+++ src/puzzles/astro/solver/Astro.java
@@ -1,9 +1,26 @@
 package puzzles.astro.solver;
 
+import puzzles.astro.model.AstroConfig;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Astro {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         if (args.length != 1) {
             System.out.println("Usage: java Astro filename");
+        } else{
+            try{
+                String filename = args[0];
+
+                AstroConfig startConfig = new AstroConfig(filename);
+
+                System.out.println(startConfig);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
